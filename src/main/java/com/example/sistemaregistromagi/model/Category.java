3cea787be7abb30465cb88a69adb89c240c5641d
Fjzomoo7! <felipeoviedomaltez@gmail.com>
2023-02-17 13:33:31 -0600

    commitcategoryenramita1

---
+++ src/main/java/com/example/sistemaregistromagi/model/Category.java
@@ -0,0 +1,31 @@
+package com.example.sistemaregistromagi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue
+    private String ID;
+    private String Name;
+
+    public void setID(String ID) {
+        this.ID = ID;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+
+    public String getName() {
+        return Name;
+    }
+}
